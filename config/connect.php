9e0df5745545aa9bf28d9d5c83d4e501d73c659e
oxhey <oxhey@msn.com>
2016-02-25 20:49:37 +0000

    port

--- config/connect.php
+++ config/connect.php
@@ -1,17 +1,19 @@
 <?php
 
-$servername = "evs.database.windows.net";
-$username = "dbadministrator";
-$password = "Ramljane1";
+$servername = "eu-cdbr-azure-west-d.cloudapp.net";
+$username = "b675727a3851c7";
+$password = "886747ac";
 $dbname = "evs";
+$port = "3306";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 
 ?>
 
