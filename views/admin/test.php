5fea0d57678da7ea81e0e1d77038bb804b53f969
oxhey <oxhey@msn.com>
2016-02-28 11:20:43 +0000

    LiveTest v2
    another update to get it working

--- views/admin/test.php
+++ views/admin/test.php
@@ -18,8 +18,6 @@ if (isset($_SESSION['User_Role_ID']))
 	{
     
     $ts = $_POST["testset"];
-	
-	require "../../config/connect.php";
     
     mysqli_query($conn, "UPDATE test_set SET isOpen = 1 WHERE id=$ts");
     //$last_id = mysqli_insert_id($conn);
@@ -39,22 +37,15 @@ echo '
             </div>
         </div>
         <div class="row">
-               <form method="POST" name="openTest" action="">';
+               <form method="POST" name="opentest" action="">';
         
         getTestSet();
 
         echo '
-            <div class="col s12">
-                <p><a href="addSet.php" class="waves-effect waves-light btn-large yellow darken-4 btn-width">New Test Set</a></p>
-                <p><a href="add.php" class="waves-effect waves-light btn-large blue darken-4 btn-width">New Question</a></p>
-                <p><a class="waves-effect waves-light btn-large red darken-4 btn-width">Edit Test</a></p>
-                <p><a class="waves-effect waves-light btn-large green darken-4 btn-width">View Reports</a></p>
-            </div>
         </div>
-    </div>
-        
-         <input type="submit" class="waves-effect waves-light btn blue darken-3" name="opentest" value="Add Question">
-    </form>';
+         <input type="submit" class="waves-effect waves-light btn blue darken-3" name="opentest" value="Open Test">
+    </form>
+    </div>';
 	
         
 
